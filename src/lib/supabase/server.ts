3f3c87644d29a6e0be6d8de68675d80720ca8e3d
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-09-08 19:09:03 +0700

    replace admin to manager

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -3,17 +3,17 @@ import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 type CreateClientOptions = {
-  isAdmin?: false;
+  isManager?: false;
 };
 
-export async function createClient({ isAdmin = false }: CreateClientOptions) {
+export async function createClient({ isManager = false }: CreateClientOptions) {
   const cookieStore = await cookies();
   const { SUPABASE_URL, SUPABASE_ANON_KEY, SUPABASE_SERVICE_ROLE_KEY } =
     environment;
 
   return createServerClient(
     SUPABASE_URL!,
-    isAdmin ? SUPABASE_SERVICE_ROLE_KEY! : SUPABASE_ANON_KEY!,
+    isManager ? SUPABASE_SERVICE_ROLE_KEY! : SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
