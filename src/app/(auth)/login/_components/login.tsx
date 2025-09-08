6928a81b3806631c912ec545a9e1a09f699f0309
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-09-08 17:13:11 +0700

    refactor code in login form and add input form component

--- src/app/(auth)/login/_components/login.tsx
+++ src/app/(auth)/login/_components/login.tsx
@@ -1,4 +1,5 @@
-"use client"
+"use client";
+import FormInput from "@/components/common/form-input";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -9,13 +10,7 @@ import {
 } from "@/components/ui/card";
 import {
   Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import { INITIAL_LOGIN_FORM } from "@/constants/auth-constants";
 import { LoginForm, loginSchema } from "@/validations/auth-validations";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -24,63 +19,40 @@ import { useForm } from "react-hook-form";
 export default function Login() {
   const form = useForm<LoginForm>({
     resolver: zodResolver(loginSchema),
-    defaultValues: INITIAL_LOGIN_FORM
+    defaultValues: INITIAL_LOGIN_FORM,
   });
 
-  const onSubmit = form.handleSubmit(async (data) => {})
+  const onSubmit = form.handleSubmit(async (data) => {});
 
   return (
     <Card>
       <CardHeader className="text-center">
-        <CardTitle className="text-xl">
-          Welcome
-        </CardTitle>
+        <CardTitle className="text-xl">Welcome</CardTitle>
         <CardDescription>Log in with your email to continue.</CardDescription>
       </CardHeader>
       <CardContent>
         <Form {...form}>
           <form onSubmit={onSubmit} className="space-y-4">
-            <FormField
-              control={form.control}
+            <FormInput
+              form={form}
               name="email"
-              render={({ field: { ...rest } }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...rest}
-                      type="email"
-                      placeholder="Your email"
-                      autoComplete="off"
-                    />
-                  </FormControl>
-                  <FormMessage className="text-xs" />
-                </FormItem>
-              )}
+              label="Email"
+              placeholder="Your email"
+              type="email"
             />
-            <FormField
-              control={form.control}
+            <FormInput
+              form={form}
               name="password"
-              render={({ field: { ...rest } }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...rest}
-                      type="password"
-                      placeholder="Your Password"
-                      autoComplete="off"
-                    />
-                  </FormControl>
-                  <FormMessage className="text-xs" />
-                </FormItem>
-              )}
+              label="Password"
+              placeholder="Your password"
+              type="password"
             />
-            <Button type="submit" className="w-full">Login</Button>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
           </form>
         </Form>
       </CardContent>
     </Card>
   );
 }
-
