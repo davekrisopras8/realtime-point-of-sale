c9a161f933f21fcec787d47ccf411a7437b4cd43
Dave Krisopras Essanto <davemantul123456@gmail.com>
2025-09-23 14:16:30 +0700

    fix: age cookies

--- src/app/(dashboard)/manager/user/actions.ts
+++ src/app/(dashboard)/manager/user/actions.ts
@@ -166,7 +166,7 @@ export async function updateUser(prevState: AuthFormState, formData: FormData) {
       httpOnly: false,
       secure: process.env.NODE_ENV === "production",
       sameSite: "strict",
-      maxAge: 60 * 60 * 24 * 7, // 7 days
+      maxAge: 60 * 60 * 24 * 365,
     });
   }
 
